470c3748bef12190e8ea628f458e0463328a8f80
Johannes Natter <office@dsp-crowd.com>
2024-03-06 15:33:53 +0100

    main: Add ProcessingCore sources

---
+++ main/Supervising.cpp
@@ -0,0 +1,119 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 06.03.2024
+
+  Copyright (C) 2024-now Authors and www.dsp-crowd.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Supervising.h"
+#include "SystemDebugging.h"
+
+#define dForEach_ProcState(gen) \
+		gen(StStart) \
+		gen(StMain) \
+		gen(StTmp) \
+
+#define dGenProcStateEnum(s) s,
+dProcessStateEnum(ProcState);
+
+#if 1
+#define dGenProcStateString(s) #s,
+dProcessStateStr(ProcState);
+#endif
+
+using namespace std;
+
+#define LOG_LVL	0
+
+Supervising::Supervising()
+	: Processing("Supervising")
+	, mStartMs(0)
+	, mCounter(0)
+{
+	mState = StStart;
+}
+
+/* member functions */
+
+Success Supervising::process()
+{
+	//uint32_t curTimeMs = millis();
+	//uint32_t diffMs = curTimeMs - mStartMs;
+	//Success success;
+#if 0
+	dStateTrace;
+#endif
+	switch (mState)
+	{
+	case StStart:
+
+		dbgStart();
+
+		mState = StMain;
+
+		break;
+	case StMain:
+
+		++mCounter;
+
+		// Wait some cycles
+		if (mCounter < 600)
+			break;
+
+		return Positive;
+
+		break;
+	case StTmp:
+
+		break;
+	default:
+		break;
+	}
+
+	return Pending;
+}
+
+void Supervising::dbgStart()
+{
+	SystemDebugging *pDbg;
+
+	pDbg = SystemDebugging::create(this);
+	if (!pDbg)
+	{
+		procErrLog(-1, "could not create process");
+		return;
+	}
+
+	pDbg->procTreeDisplaySet(false);
+	SystemDebugging::levelLogSet(3);
+
+	start(pDbg);
+}
+
+void Supervising::processInfo(char *pBuf, char *pBufEnd)
+{
+#if 1
+	dInfo("State\t\t\t%s\n", ProcStateString[mState]);
+#endif
+}
+
+/* static functions */
+
